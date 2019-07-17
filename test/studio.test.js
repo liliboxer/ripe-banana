8f222fd6ef42e38055dd51a32b838db1da2468a7
Lili Boxer <ninaliboxer@gmail.com>
2019-07-17 15:00:10 -0700

    path for get all studios working

--- test/studio.test.js
+++ test/studio.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
+const Studio = require('../lib/models/Studio')
+
 describe('studio routes', () => {
   beforeAll(() => {
     connect();
@@ -18,28 +20,22 @@ describe('studio routes', () => {
     return mongoose.connection.close();
   });
 
-  it('get all studios', () => {
+  it('GET all studios', async() => {
+    const studios = await Studio.create([
+      { name: 'Studio Ghibli', address: { city: 'Tokyo',state: 'Tokyo', country: 'Japan'}},
+      { name: 'Wes Anderson', address: { city: 'Cool',state: 'Idk', country: 'USA'}},
+      { name: 'Some Other Studio', address: { city: 'IDK',state: 'idc', country: 'wherever'}}
+    ])
     return request(app)
       .get('/api/v1/studios')
-      .send({
-        name: 'Studio Ghibli',
-        address: {
-          city: 'Tokyo',
-          state: 'Tokyo',
-          country: 'Japan'
-        }
-      })
       .then(res => {
-        expect(res.body).toEqual({
-          __v: 0,
-          _id: expect.any(String),
-          name: 'Studio Ghibli',
-          address: {
-            city: 'Tokyo',
-            state: 'Tokyo',
-            country: 'Japan'
-          }
+        const studiosJSON = JSON.parse(JSON.stringify(studios));
+        studiosJSON.forEach(studio => {
+          expect(res.body).toContainEqual(studio)
         })
       })
   })
+  // it('POST studio', () => {
+  //   .post('/api/v1/studios')
+  // })
 })
