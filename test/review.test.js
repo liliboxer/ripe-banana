1ba3439577b3d4c1a381674dded4187ff2acd809
Lili Boxer <ninaliboxer@gmail.com>
2019-07-19 09:59:40 -0700

    make 101 reviews and get 100 back

--- test/review.test.js
+++ test/review.test.js
@@ -30,14 +30,27 @@ describe('review routes', () => {
     studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'Max Studio', address: { city: 'Portland', state: 'Oregon', country: 'USA' } })));
     actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'lili', dob: '1992-03-07', pob: 'somewhere' })));
     film = JSON.parse(JSON.stringify(await Film.create({ title: 'Princess Mononoke', released: 1990, studio: studio._id, cast: { role: 'ugh', actor: actor._id } })));
-    reviewer = JSON.parse(JSON.stringify(await Reviewer.create({ name: 'max', company: 'talks too loud'}));
+    reviewer = JSON.parse(JSON.stringify(await Reviewer.create({ name: 'max', company: 'talks too loud' })));
   });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
 
-  it('GET reviews, 100 most recent, film {_id, title} ', () => {
+  it('GET reviews, 100 most recent, film {_id, title} ', async() => {
+    await Promise.all([...Array(101)].map((i) => {
+      return Review.create({
+        rating: 8,
+        reviewer: reviewer._id,
+        review: `this is review #${i}`,
+        film: film._id
+      });
+    }));
 
+    return request(app)
+      .get('/api/v1/reviews')
+      .then(res => {
+        expect(res.body).toHaveLength(100);
+      });
   });
 });
